2340b2848cfb7789a6485efa4efee83b7e0f847b
Brian Holley <brian.holley@hotmail.com>
2017-03-10 22:44:58 -0800

    Add Invader Zim show

--- tv-quotes.js
+++ tv-quotes.js
@@ -4,7 +4,7 @@ Module.register("tv-quotes",{
 
     // Default configuration is all shows
     defaults: {
-        shows: [ "doctorwho", "futurama", "kingofthehill", "thesimpsons" ],
+        shows: [ "doctorwho", "futurama", "kingofthehill", "invaderzim", "thesimpsons" ],
         timeShown: 60, // how long is the quote shown, in seconds
         fadeSpeed: 2, // how long does it take to fade in/out, in seconds
     },
